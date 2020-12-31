cc47f543141ef5611e983ee280447dd8b2952b27
Dayne Dewar <daynedewar@yahoo.com>
2020-12-31 22:44:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index do |list, index|
+  array.each_with_index { |list, index|
     array[list] = index
-  end
+  }
+  array
 end
