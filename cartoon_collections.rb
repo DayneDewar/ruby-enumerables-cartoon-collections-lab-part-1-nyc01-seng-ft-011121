9ae7de8a6910ddc8c10810031cc359c1bf8d315e
Dayne Dewar <daynedewar@yahoo.com>
2020-12-31 22:49:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   array.each_with_index do |list, index|
     array[list] = index
-  }
-  
+    puts "#{index + 1}. #{list}"
+  end
 end
